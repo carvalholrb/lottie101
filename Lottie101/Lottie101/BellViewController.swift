af7a9b115bb6b39b05b636a518e7baea58671226
Laura Carvalho <carvalholrb@gmail.com>
2019-04-15 15:16:06 -0300

    Add bell animation
    - Add animations in json
    - Add animation view to view controller, setup its constraints and plays animation on view did load

--- Lottie101/Lottie101/BellViewController.swift
+++ Lottie101/Lottie101/BellViewController.swift
@@ -7,10 +7,37 @@
 //
 
 import UIKit
+import Lottie
 
 final class BellViewController: UIViewController {
+
+    private enum Constants {
+        static let notification = "notification"
+        static let animationSide: CGFloat = 300.0
+    }
+
+    private lazy var bellAnimation: AnimationView = {
+        let animationView = AnimationView(name: Constants.notification)
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.loopMode = .loop
+        return animationView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(bellAnimation)
+        setupAnimationConstraints()
+
+        bellAnimation.play()
+    }
+
+    private func setupAnimationConstraints() {
+        NSLayoutConstraint.activate([
+            bellAnimation.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            bellAnimation.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            bellAnimation.widthAnchor.constraint(equalToConstant: Constants.animationSide),
+            bellAnimation.heightAnchor.constraint(equalToConstant: Constants.animationSide)
+            ])
     }
 }
-
