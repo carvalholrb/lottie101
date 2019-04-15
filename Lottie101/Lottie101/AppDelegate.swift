379f6d3dbefedd0b0b2515a994cdb96cb8c59060
Laura Carvalho <carvalholrb@gmail.com>
2019-04-15 13:33:31 -0300

    Add new single view project

---
+++ Lottie101/Lottie101/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Lottie101
+//
+//  Created by Laura Carvalho on 4/15/19.
+//  Copyright © 2019 ArcTouch. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Lottie101/Lottie101/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Lottie101
+//
+//  Created by Laura Carvalho on 4/15/19.
+//  Copyright © 2019 ArcTouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
